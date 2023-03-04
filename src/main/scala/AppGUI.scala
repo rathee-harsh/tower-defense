4afe7deb8250cf56616f5d0201df3d508e262697
Harsh Rathee <harshrathee1305@gmail.com>
2023-03-04 14:26:41 +0200

    Adding the bottom panel in the GUI

--- src/main/scala/AppGUI.scala
+++ src/main/scala/AppGUI.scala
@@ -1,13 +1,73 @@
 import scala.swing.*
+import javax.swing.JFrame
+import javax.swing.JPanel
+import java.awt.BorderLayout
+import java.awt.Color
+import java.awt.Graphics
+import javax.imageio.ImageIO
+import scala.collection.mutable.Buffer
+import java.io.File
+
+
+class TowerPanel() extends Panel:
+  override def paintComponent(g : Graphics2D) =
+    val image = ImageIO.read(new File("assets/lockedItem.png"))
+    g.drawImage(image, 0, 0, 100, 100, null)
+end TowerPanel
+
+
+class StatusPanel() extends Panel:
+  override def paintComponent(g : Graphics2D) =
+    val img = ImageIO.read(new File("assets/test.png") )
+    g.drawImage(img, 0, 15, 200, 70, null)
+
+    val health = ImageIO.read(new File("assets/health.png"))
+    g.drawImage(health, 5, 20, 20, 20, null)
+    g.setColor(Color.red)
+    g.fillRect(30, 25, 100, 8)
+
+    val coins = ImageIO.read(new File("assets/coins.png"))
+    g.drawImage(coins, 5, 50, 25, 25, null)
+    g.setColor(Color.yellow)
+    g.drawString("550", 40, 67)
+
+end StatusPanel
+
 
 object AppGUI extends SimpleSwingApplication:
-  // Main components:
-  val allPartsTogether = BoxPanel(Orientation.Vertical)
-  val frame = MainFrame()
-  frame.contents = allPartsTogether
-  frame.title = "Tower Defence"
+  val WIDTH = 900
+  val HEIGHT = 700
+  val firstButton  = Button("Press me, please")( () )
+  val secondButton = Button("No, press ME!")( () )
+  val prompt = Label("Press one of the buttons.")
 
-  // A method that returns the app’s main window:
-  def top = this.frame
+  val spell = new TowerPanel
+  val towers= Buffer[TowerPanel](new TowerPanel, new TowerPanel, new TowerPanel, new TowerPanel)
+
+  val status = new StatusPanel
+  val towersAndSpells = BoxPanel(Orientation.Horizontal)
+  towersAndSpells.contents ++= towers
+  towersAndSpells.contents += spell
+
+  val bottomMenu = BoxPanel(Orientation.Horizontal)
+  bottomMenu.contents += status
+  bottomMenu.contents += towersAndSpells
+
+  val root = BoxPanel(Orientation.Vertical)
+  root.contents += prompt
+  root.contents += bottomMenu
+
+  val gameWindow = new MainFrame:
+    title = "Level 1"
+    contents = root
+    size = new Dimension(WIDTH, HEIGHT)
+    resizable = false
+
+
+  def top = this.gameWindow
 
 end AppGUI
+
+
+
+
