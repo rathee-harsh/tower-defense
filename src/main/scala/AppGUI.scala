685af6a369b22b5dbabc45be48950236797e8526
Harsh Rathee <harshrathee1305@gmail.com>
2023-04-08 02:27:19 +0300

    Buttons at the game over screen now work and the player can move on to the next levels after completing the current one.

--- src/main/scala/AppGUI.scala
+++ src/main/scala/AppGUI.scala
@@ -1,3 +1,5 @@
+import AppGUI.level
+
 import scala.swing.*
 import javax.swing.{BorderFactory, BoxLayout, ImageIcon, JFrame, JPanel}
 import java.awt.BorderLayout
@@ -14,40 +16,31 @@ import scala.collection.mutable.*
 object AppGUI extends SimpleSwingApplication:
 
   var level = 1
-
-  private def formGridCoordinates(point: Point): GridPos =
-    val x = point.getX
-    val y = point.getY
-    val relativeX = x
-    val relativeY = y - topBar.size.getHeight
-    GridPos(math.floor(relativeX/root.size.getWidth * COLS), math.floor(relativeY/mainGame.size.getHeight * ROWS))
-  end formGridCoordinates
-
-  private def createButtons(towerButtons: Map[String, String]) =
-    val buttons = Buffer[Button]()
-    for (buttonName, imagePath) <- towerButtons do
-      val image = ImageIO.read(new File(imagePath))
-      buttons += new Button("") {
-        this.name = buttonName
-        icon = new ImageIcon(image.getScaledInstance(50, 50, 2))
-        preferredSize = new Dimension(icon.getIconWidth, icon.getIconHeight)
-        focusable = false
-      }
-    buttons.toSeq
-
+  var exitGame = false
   val towerButtons = Map[String, String]("cannon" -> CANNON_IMAGE_PATH, "bomber" -> BOMBER_IMAGE_PATH, "collector" -> COLLECTOR_IMAGE_PATH, "archer" -> ARCHER_IAMGE_PATH)
-
-  var game = Game(level)
+  var gameLevel = Game(level)
   val buttons = this.createButtons(towerButtons)
+  var nextLevelStarted = false
 
-  val buttonPanel = new FlowPanel
-  buttonPanel.contents ++= this.buttons
-  val bottomLeft = new BottomPanel(game):
-    preferredSize = new Dimension(100, 60)
-  val bottomMenu = new BorderPanel:
-    layout(buttonPanel) = BorderPanel.Position.East
-    layout(bottomLeft) = BorderPanel.Position.West
-  bottomMenu.preferredSize = Dimension(WIDTH, 60)
+  val restart = new Button():
+    name = "restart"
+    icon = new ImageIcon(ImageIO.read(new File(RESTART_IMAGE_PATH)).getScaledInstance(50, 50, 2))
+    preferredSize = new Dimension(icon.getIconWidth, icon.getIconHeight)
+    focusable = false
+    opaque = false
+
+  val next = new Button():
+    name = "next"
+    icon = new ImageIcon(ImageIO.read(new File(NEXT_IAMGE_PATH)).getScaledInstance(50, 50, 2))
+    preferredSize = new Dimension(icon.getIconWidth, icon.getIconHeight)
+    focusable = false
+    opaque = false
+
+  val exit = new Button():
+      name = "exit"
+      icon = new ImageIcon(ImageIO.read(new File(EXIT_IMAGE_PATH)).getScaledInstance(50, 50, 2))
+      preferredSize = new Dimension(icon.getIconWidth, icon.getIconHeight)
+      focusable = false
 
   val pause = new PauseScreen("Game Paused", Vector(45, 45, 20, 20)):
     val pauseOptions = new BoxPanel(Orientation.Vertical):
@@ -68,99 +61,143 @@ object AppGUI extends SimpleSwingApplication:
     visible = false
 
   val gameOverOptions = new BoxPanel(Orientation.Vertical):
-    val restart = new Button():
-      name = "restart"
-      icon = new ImageIcon(ImageIO.read(new File(RESTART_IMAGE_PATH)).getScaledInstance(50, 50, 2))
-      preferredSize = new Dimension(icon.getIconWidth, icon.getIconHeight)
-      focusable = false
-      opaque = false
     val margin1 = new FlowPanel:
       preferredSize = Dimension(PAUSE_SCREEN_WIDTH, (PAUSE_SCREEN_HEIGHT * 0.05).toInt)
-      opaque = false
-    val next = new Button():
-      name = "next"
-      icon = new ImageIcon(ImageIO.read(new File(NEXT_IAMGE_PATH)).getScaledInstance(50, 50, 2))
-      preferredSize = new Dimension(icon.getIconWidth, icon.getIconHeight)
-      focusable = false
       opaque = false
     val margin2 = new FlowPanel:
       preferredSize = Dimension(PAUSE_SCREEN_WIDTH, (PAUSE_SCREEN_HEIGHT * 0.05).toInt)
       opaque = false
-    val exit = new Button():
-      name = "exit"
-      icon = new ImageIcon(ImageIO.read(new File(EXIT_IMAGE_PATH)).getScaledInstance(50, 50, 2))
-      preferredSize = new Dimension(icon.getIconWidth, icon.getIconHeight)
-      focusable = false
-      opaque = false
+    opaque = false
     contents ++= Seq(restart, margin1, next, margin2, exit)
     opaque = false
   end gameOverOptions
+
 
   val gameOver = new PauseScreen("Game Over", Vector(20, 20, 30, 30)):
     preferredSize = Dimension(500, 500)
     layout(gameOverOptions) = BorderPanel.Position.Center
     visible = false
 
-  val mainGame = new mainMap(game)
-  mainGame.contents ++= Seq(pause, gameOver)
+  private def startNewLevel(level: Int): Unit =
+    gameLevel = new Game(level)
+    topBar = createTopMenu
+    mainGame = createMainGame
+    bottomMenu = createBottomMenu(buttons.toVector)
+    root = newRoot
+    gameOver.visible = false
+    nextLevelStarted = true
+    gameWindow.contents = root
 
-  val topBar = new TopBar(game)
-  topBar.preferredSize = Dimension(WIDTH, 100)
+  private def formGridCoordinates(point: Point): GridPos =
+    val x = point.getX
+    val y = point.getY
+    val relativeX = x
+    val relativeY = y - topBar.size.getHeight
+    GridPos(math.floor(relativeX/root.size.getWidth * COLS), math.floor(relativeY/mainGame.size.getHeight * ROWS))
+  end formGridCoordinates
 
+  private def createButtons(towerButtons: Map[String, String]) =
+    val buttons = Buffer[Button]()
+    for (buttonName, imagePath) <- towerButtons do
+      val image = ImageIO.read(new File(imagePath))
+      buttons += new Button("") {
+        this.name = buttonName
+        icon = new ImageIcon(image.getScaledInstance(50, 50, 2))
+        preferredSize = new Dimension(icon.getIconWidth, icon.getIconHeight)
+        focusable = false
+      }
+    buttons.toSeq
+  end createButtons
 
-  val root = new BorderPanel:
+  private def createTopMenu =
+    new TopBar(gameLevel):
+      preferredSize = Dimension(WIDTH, 100)
+
+  private def createBottomMenu(buttons: Vector[Button]) =
+    val buttonPanel = new FlowPanel
+    buttonPanel.contents ++= buttons
+    val bottomLeft = new BottomPanel(gameLevel):
+      preferredSize = new Dimension(100, 60)
+    val bottomMenu = new BorderPanel:
+      layout(buttonPanel) = BorderPanel.Position.East
+      layout(bottomLeft) = BorderPanel.Position.West
+      preferredSize = Dimension(WIDTH, 60)
+    bottomMenu
+  end createBottomMenu
+
+  private def createMainGame =
+    new mainMap(gameLevel):
+      contents ++= Seq(pause, gameOver)
+  end createMainGame
+
+  var topBar = createTopMenu
+  var mainGame= createMainGame
+  var bottomMenu = createBottomMenu(buttons.toVector)
+
+  private def newRoot = new BorderPanel:
     add(topBar,BorderPanel.Position.North)
     add(mainGame,BorderPanel.Position.Center)
     add(bottomMenu,BorderPanel.Position.South)
     focusable = true
+    listenTo(restart)
+    listenTo(next)
+    listenTo(exit)
+
     listenTo(this.keys)
     listenTo(this.mouse.moves)
     listenTo(this.mouse.clicks)
-    buttons.foreach( this.listenTo(_) )
+    buttons.foreach( listenTo(_) )
     reactions += {
-      case KeyPressed(_,  Key.P, _, _)  if !game.isOver=>
-        game.isPaused = !game.isPaused
+      case KeyPressed(_,  Key.P, _, _)  if !gameLevel.isOver=>
+        gameLevel.isPaused = !gameLevel.isPaused
 
-      case MouseMoved(_, point, _) if !game.isPaused && !game.isOver =>
+      case MouseMoved(_, point, _) if !gameLevel.isPaused && !gameLevel.isOver =>
         if point.getY > topBar.size.getHeight && point.getY < (this.size.getHeight - bottomMenu.size.getHeight) then
           PickedUpTower.cursorOnGame = true
           PickedUpTower.location = formGridCoordinates(point)
         else
           PickedUpTower.cursorOnGame = false
 
-      case MouseClicked(component, point, _, _, _) if !game.isPaused && !game.isOver =>
+      case MouseClicked(component, point, _, _, _) if !gameLevel.isPaused && !gameLevel.isOver =>
         if PickedUpTower.tower.isDefined then
           val location = formGridCoordinates(point)
-          game.gridMap.get(location) match
-            case Some(direction) if !game.getTowerLocations.contains(location) =>
+          gameLevel.gridMap.get(location) match
+            case Some(direction) if !gameLevel.getTowerLocations.contains(location) =>
               if direction == Direction.Placable && !PickedUpTower.isCollector then
                 PickedUpTower.tower.get.move(location)
-                game.addTower(PickedUpTower.tower.get)
+                gameLevel.addTower(PickedUpTower.tower.get)
               else if direction == Direction.Forest && PickedUpTower.isCollector then
                 PickedUpTower.tower.get.move(location)
-                game.addTower(PickedUpTower.tower.get)
+                gameLevel.addTower(PickedUpTower.tower.get)
             case _ => ()
           PickedUpTower.tower = None
           PickedUpTower.cursorOnGame = false
 
-      case buttonClicked: ButtonClicked if !game.isPaused && !game.isOver =>
+      case buttonClicked: ButtonClicked if !gameLevel.isPaused =>
         buttonClicked.source.name match
-          case "cannon" =>
-            PickedUpTower.tower = Some(new Cannon( game, 1, GridPos(10, 4), Direction.North, 10))
+          case "cannon" if !gameLevel.isOver =>
+            PickedUpTower.tower = Some(new Cannon( gameLevel, 1, GridPos(10, 4), Direction.North, 10))
             PickedUpTower.isCollector = false
-          case "archer" =>
-            PickedUpTower.tower = Some(new Archer( game, 1, GridPos(10, 4), Direction.North, 10))
+          case "archer" if !gameLevel.isOver =>
+            PickedUpTower.tower = Some(new Archer( gameLevel, 1, GridPos(10, 4), Direction.North, 10))
             PickedUpTower.isCollector = false
-          case "bomber" =>
-            PickedUpTower.tower = Some(new Bomber( game, 1, GridPos(10, 4), Direction.North, 25))
+          case "bomber" if !gameLevel.isOver =>
+            PickedUpTower.tower = Some(new Bomber( gameLevel, 1, GridPos(10, 4), Direction.North, 25))
             PickedUpTower.isCollector = false
-          case "collector" =>
-            PickedUpTower.tower = Some(new Collector(game, 1, GridPos(10, 4), 15))
+          case "collector" if !gameLevel.isOver =>
+            PickedUpTower.tower = Some(new Collector(gameLevel, 1, GridPos(10, 4), 15))
             PickedUpTower.isCollector = true
-          case _ => ()
+          case "restart" =>
+            startNewLevel(level)
+          case "next" =>
+            level += 1
+            startNewLevel(level)
+          case "exit" => exitGame = true
     }
 
-  val gameWindow = new MainFrame:
+  var root = newRoot
+
+  var gameWindow = new MainFrame:
     title = "Level 1"
     contents = root
     preferredSize = new Dimension(WIDTH, HEIGHT)
@@ -171,13 +208,18 @@ object AppGUI extends SimpleSwingApplication:
 
   val listener = new ActionListener():
       def actionPerformed(e: java.awt.event.ActionEvent) =
-        if game.isOver then
+        if exitGame then
+          gameWindow.dispose()
+        else if nextLevelStarted then
+          root.repaint()
+          nextLevelStarted = false
+        else if gameLevel.isOver then
           gameOver.visible = true
-        else if game.isPaused then
+        else if gameLevel.isPaused then
           pause.visible =  true
           pause.repaint()
         else
-          game.advance()
+          gameLevel.advance()
           topBar.repaint()
           mainGame.repaint()
           bottomMenu.repaint()
